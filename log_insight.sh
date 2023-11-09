6707305042a9f4bb1a14b278faf13904d051a11c
gehrychiang <jscblack@163.com>
2023-11-09 14:53:25 +0800

    fix: 修复typo

--- log_insight.sh
+++ log_insight.sh
@@ -2,5 +2,5 @@ TRACE_ID=$1
 LOG_PATH=$2
 MERGE_LOG_PATH=$3
 grep $(TRACE_ID) $(LOG_PATH)/observer* $(LOG_PATH)/election* $(LOG_PATH)/rootservice* \
-    | sed 's/:/ /  awk '{tmp=$1; $1=$2; $2=$3; $3=$4; $4=tmp; print $0}' \
+    | sed 's/:/ /'  awk '{tmp=$1; $1=$2; $2=$3; $3=$4; $4=tmp; print $0}' \
     | sort > ${MERGE LOG PATH}
