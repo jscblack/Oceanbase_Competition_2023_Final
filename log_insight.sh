416c47aebde5b9db3f1c7f307a573b248ba08e26
gehrychiang <jscblack@163.com>
2023-11-09 16:26:57 +0800

    fix: 修复log_insight脚本

--- log_insight.sh
+++ log_insight.sh
@@ -1,6 +1,9 @@
-TRACE_ID=$1
-LOG_PATH=$2
-MERGE_LOG_PATH=$3
-grep $(TRACE_ID) $(LOG_PATH)/observer* $(LOG_PATH)/election* $(LOG_PATH)/rootservice* \
-    | sed 's/:/ /'  awk '{tmp=$1; $1=$2; $2=$3; $3=$4; $4=tmp; print $0}' \
-    | sort > ${MERGE LOG PATH}
+#!/bin/bash
+TRACE_ID="$1"
+LOG_PATH="$2"
+MERGE_LOG_PATH=${LOG_PATH}/${TRACE_ID}.log
+touch ${MERGE_LOG_PATH} \
+    &&
+grep ${TRACE_ID} ${LOG_PATH}/observer* ${LOG_PATH}/election* ${LOG_PATH}/rootservice* \
+    | sed 's/:/ /' | awk '{tmp=$1; $1=$2; $2=$3; $3=$4; $4=tmp; print $0}' \
+    | sort > ${MERGE_LOG_PATH}
